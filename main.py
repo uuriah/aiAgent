e406167e66ad7543e691bdaf754986f444f5486c
uuriah <132951774+uuriah@users.noreply.github.com>
2026-01-14 17:04:45 -0800

    Add imports

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import argparse
+from prompts import system_prompt
+from call_function import available_functions
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -19,7 +21,15 @@ args = parser.parse_args()
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
+response = client.models.generate_content(
+    model='gemini-2.5-flash', 
+    contents=messages,
+    config=types.GenerateContentConfig(
+        system_instruction=system_prompt,
+        temperature=0,
+        tools=[available_functions]
+        )
+    )
 
 if args.verbose:
     print(f"User prompt: {args.user_prompt}")
@@ -29,4 +39,9 @@ if args.verbose:
     else:
         raise RuntimeError("Request failed!")
 
-print(response.text)
+if response.function_calls:
+    function_calls = response.function_calls
+    for function_call in function_calls:
+        print(f"Calling function: {function_call.name}({function_call.args})")
+else:
+    print(response.text)
