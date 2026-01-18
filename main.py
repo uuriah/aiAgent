0eccbaf361f57d01d7abf3e84f4a38c6679dd48a
uuriah <132951774+uuriah@users.noreply.github.com>
2026-01-17 20:53:35 -0800

    Allow llm to run 20 iterations before failing

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from google import genai
 from google.genai import types
 import argparse
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -20,28 +21,58 @@ parser.add_argument("--verbose", action="store_true", help="Enable verbose outpu
 args = parser.parse_args()
 
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+i = 0
 
-response = client.models.generate_content(
-    model='gemini-2.5-flash', 
-    contents=messages,
-    config=types.GenerateContentConfig(
-        system_instruction=system_prompt,
-        temperature=0,
-        tools=[available_functions]
+for _ in range(20):
+    i += 1
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            temperature=0,
+            tools=[available_functions]
+            )
         )
-    )
+    
+    if response.candidates:
+        for candidate in response.candidates:
+            if candidate.content:
+                messages.append(candidate.content)
 
-if args.verbose:
-    print(f"User prompt: {args.user_prompt}")
-    if response.usage_metadata != None:
-        print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
-        print("Response tokens: ", response.usage_metadata.candidates_token_count)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        if response.usage_metadata != None:
+            print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+            print("Response tokens: ", response.usage_metadata.candidates_token_count)
+
+    if response.function_calls:
+        function_result_list = []
+
+        function_calls = response.function_calls
+        for function_call in function_calls:
+            function_call_result = call_function(function_call)
+
+            if not function_call_result.parts:
+                raise Exception(f"Error: Function call {function_call} resulted in empty parts list")
+            
+            if function_call_result.parts[0].function_response is None:
+                raise Exception("Error: Function response was None")
+            
+            if function_call_result.parts[0].function_response.response is None:
+                raise Exception("Error: Function response.response is None")
+            
+            function_result_list.append(function_call_result.parts[0])
+
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+
+        messages.append(types.Content(role="user", parts=function_result_list))
+        
     else:
-        raise RuntimeError("Request failed!")
+        print(response.text)
+        break
 
-if response.function_calls:
-    function_calls = response.function_calls
-    for function_call in function_calls:
-        print(f"Calling function: {function_call.name}({function_call.args})")
-else:
-    print(response.text)
+if i == 20:
+    print("Maximum number of iterations reached.")
+    exit(1)
